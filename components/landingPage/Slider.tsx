3dc3598b18fec7cec66573063091a3e27799a537
I'm Naman Bansal <98607723+Namanb454@users.noreply.github.com>
2025-07-20 21:02:36 +0530

    remove token from slider

--- components/landingPage/Slider.tsx
+++ components/landingPage/Slider.tsx
@@ -82,31 +82,31 @@ export const BrandSlider: React.FC<BrandSliderProps> = ({ variant, className = '
     const client = [
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/12C.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsLzEyQy5tcDQiLCJpYXQiOjE3NDc4MjkyNTEsImV4cCI6MjA2MzE4OTI1MX0.YGHOcYQg7soryXIsQ79wY2KlH3ADuBGBvrOgMlt3hTU'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/Ai2.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL0FpMi5tcDQiLCJpYXQiOjE3NDc4MjkzNTEsImV4cCI6MjA2MzE4OTM1MX0.01eA_bnftM9JBFyh7_U6W9KIn-GHWWgrF2Ydq8m4FsE'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/AI.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL0FJLm1wNCIsImlhdCI6MTc0NzgyOTM2NywiZXhwIjoyMDYzMTg5MzY3fQ.vsdfpHmTorItWWBrP7aKorIalskWluDQMxhfaIFBwag'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/Bin%20laden.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL0JpbiBsYWRlbi5tcDQiLCJpYXQiOjE3NDc4MjkzOTgsImV4cCI6MjA2MzE4OTM5OH0.iX_7czzVZJpaGrpOMnC-j2TmL1vcKVzCcrkQSIaKOJg'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/himalayas.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL2hpbWFsYXlhcy5tcDQiLCJpYXQiOjE3NDc4Mjk0MTYsImV4cCI6MjA2MzE4OTQxNn0.EIEcUQOkVS9v1DmzbzTH9jbNa5EtrW9_gcUZSQuqWDc'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/LV.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL0xWLm1wNCIsImlhdCI6MTc0NzgyOTQzMiwiZXhwIjoyMDYzMTg5NDMyfQ.1sU3byvW7AdbDm8nYDx4zMM2SofG55ep6MOgyKA6-X4'
+
         },
         {
             'title': 'SaaS Development',
-            'url': 'https://zgyslfhedwkrwtdihcjt.supabase.co/storage/v1/object/sign/landing-page-scroll/mumbai.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJsYW5kaW5nLXBhZ2Utc2Nyb2xsL211bWJhaS5tcDQiLCJpYXQiOjE3NDc4Mjk0NDUsImV4cCI6MjA2MzE4OTQ0NX0.-4zrQnD8EmsvhRttTfH6isRDv4z01IxLPI-XXJVo7ZU'
+
         },
     ]
 
@@ -118,7 +118,7 @@ export const BrandSlider: React.FC<BrandSliderProps> = ({ variant, className = '
                     <div key={index} className="p-2 overflow-hidden rounded-2xl">
                         <div className="w-fit mx-auto p-2 mb-5 shadow-md shadow-indigo-500 text-left rounded-2xl overflow-hidden">
                             <video className="h-96" autoPlay muted>
-                                <source src={item.url}  className="rounded-2xl"/>
+                                {/* <source src={item.url}  className="rounded-2xl"/> */}
                             </video>
                         </div>
                     </div>
